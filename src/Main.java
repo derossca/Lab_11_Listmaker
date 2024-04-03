c28bfc0dcea47177286ac953efde2a115a575dcf
derossca <derossca@mail.uc.edu>
2024-04-03 11:20:22 -0400

    imported array list utility

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+//importing utilities
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+
     }
 }
