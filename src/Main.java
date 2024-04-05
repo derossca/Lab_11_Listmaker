8ad905637f479e50807c10cd0bbdae3d0f321af1
derossca <derossca@mail.uc.edu>
2024-04-05 08:23:39 -0400

    comments updated

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
 
         final String menu = "A - add D - delete P - print Q - quit";
-        boolean done = false;
+        boolean done = false; //putting here just in case needed
         String cmd = "";
 
         do
@@ -44,25 +44,31 @@ public class Main {
                     printItem();
                     break;
                 case "Q":
+                    //prompt user to see if they are sure they want to quit
+                    //if Yy then program ceases, if Nn program continues and prompts menu choices
                     quit();
                     break;
             }
             System.out.println();
         }
         while (quit() != true);
-
+        //quit set to false unless user input in method below is Yy
+        //program only quits when condition is met in helper method quit() below
     }
 
     private static void displayList()
     {
+        //list size cannot be zero
         if(list.size() != 0)
         {
+            //for loop that prints out list number + 1 for user readability, then list item
+            //and some space is added for readability
             for(int i = 0; i < list.size(); i++)
             {
                 System.out.printf("%1d%10s%5s", i + 1, list.get(i), " ");
             }
         }
-        else
+        else //else output that the list is empty
         {
             System.out.println("List is empty!");
         }
@@ -71,10 +77,13 @@ public class Main {
 
     private static void add()
     {
+        //declaring variable needed
         String addItem = "";
 
+        //calling method so user is prompted and inputs an item to add
         addItem = SafeInput.getNonZeroLenString(in, "Add an item to the list ");
 
+        //user input is added to the list
         list.add(addItem);
     }
 
@@ -82,31 +91,41 @@ public class Main {
     {
         int deleteItem = 0;
 
+        //getting user input while prompting for number of the list item to delete
         deleteItem = SafeInput.getRangedInt(in, "Select the number of the item to delete ", 1, 100);
+        //matching user number with array index number by subtracting 1
         deleteItem = deleteItem - 1;
 
+        //removing the item at index location given and translated
         list.remove(deleteItem);
     }
 
     private static void printItem()
     {
+        //outputs the array list
         displayList();
     }
 
+    //made this a boolean to work with the switch case Q and do while loop for entire program
     private static boolean quit()
     {
         boolean quitting = false;
 
+        //prompts user to see if they are sure they want to quit Yy or Nn
         quitting = SafeInput.getYNConfirm(in, "Are you sure you want to quit?");
 
+        //If else structure that returns true and will end program in main method
+        //should user choose Yy
         if(quitting)
         {
             quitting = true;
         }
+        //else the boolean quitting is false and program continues on
         else
         {
             quitting = false;
         }
+        //return result of quitting from if else structure
         return quitting;
     }
 }
